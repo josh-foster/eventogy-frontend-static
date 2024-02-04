4fbdaa2389ed9c8482035bacc906ed4e89398212
josh-foster <70209051+josh-foster@users.noreply.github.com>
2024-02-04 14:22:15 +0000

    add header to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import { Search, User } from "lucide-react";
+
 import "./globals.css";
+import eventogyLogo from "../../public/eventogy-logo.svg";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -20,7 +30,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="flex justify-between items-center px-4 py-2.5 bg-muted">
+          <Image priority height={24} src={eventogyLogo} alt="Eventogy Logo" />
+          <div className="relative inline-block w-6/12">
+            <Search
+              color="grey"
+              className="absolute left-4 top-1/2 transform -translate-y-1/2 pointer-events-none"
+            />
+            <Input
+              className="pl-12 w-full rounded-full"
+              type="search"
+              placeholder="Search Events, Attendees, Venues..."
+            />
+          </div>
+
+          <Popover>
+            <PopoverTrigger className="bg-background rounded-full w-10 h-10 flex justify-center items-center">
+              <User color="grey" />
+            </PopoverTrigger>
+            <PopoverContent className="mr-2">
+              test-user@gmail.com
+            </PopoverContent>
+          </Popover>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
