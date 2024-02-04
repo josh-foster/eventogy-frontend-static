98f78f48d0c47db02063a82af7aeb74c02185579
josh-foster <70209051+josh-foster@users.noreply.github.com>
2024-02-04 14:50:50 +0000

    fetch events data for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
-import { Button } from "@/components/ui/button";
+interface Data {
+  scheduled_dates: string[];
+  timezone: string;
+  location_long: number;
+  location_lat: number;
+}
 
-export default function Home() {
-  return <Button>Click me</Button>;
+interface Event {
+  id: string;
+  name: string;
+  slug: string;
+  status: number;
+  created_at: string;
+  updated_at: string;
+  deleted_at: string | null;
+  data: Data;
+}
+
+async function getEvents(): Promise<Event[]> {
+  const response = await fetch(`https://www.eventogy.com/api/events.json`, {
+    method: "GET",
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json().then((data) => data.data);
+}
+
+export default async function Home() {
+  const events = await getEvents();
+
+  console.log(events);
+
+  return <div>events</div>;
 }
