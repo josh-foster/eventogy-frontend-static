0f34cf0b7ffb7198ef63067091bb023d1fa33102
josh-foster <70209051+josh-foster@users.noreply.github.com>
2024-02-05 20:48:29 +0000

    fix card responsiveness

--- src/app/event-hub/events/components/events.tsx
+++ src/app/event-hub/events/components/events.tsx
@@ -19,7 +19,7 @@ export default async function Events() {
 
   return (
     <div className="flex justify-center px-7">
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-3">
         {events.map((event, index) => (
           <EventCard
             key={event.id}
--- src/app/event-hub/events/loading.tsx
+++ src/app/event-hub/events/loading.tsx
@@ -3,7 +3,7 @@ import SkeletonEventCard from "@/app/event-hub/events/components/skeleton-event-
 export default function Loading() {
   return (
     <div className="flex justify-center">
-      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-3">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-3">
         {Array.from({ length: 8 }).map((_, i) => (
           <SkeletonEventCard key={i} />
         ))}
